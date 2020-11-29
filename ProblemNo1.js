d8a97865a79988acb98c99af10429255ca7835e8
Bhushan Wagh <bhushanwagh6@gmail.com>
2020-11-29 21:10:49 +0530

    Code refactoring

--- ProblemNo1.js
+++ ProblemNo1.js
@@ -3,24 +3,15 @@ const loadPerMin = [10, 60, 50, 15, 20];
 const serverRunIntervalInM = 60000;
 const index = 0;
 
-const checkLoad = (l) => {
-    console.log('l', l)
-    if(l < 50){
+const checkLoad = (load) => {
+    if(load < 50){
         server = server/2;
     } else{
         server = 2*server + 1;
     }
-
-    server = Math.round(server)
-    console.log('server', server)
+    server = Math.floor(server)
 }
 
-
-for (let index = 0; index < loadPerMin.length; index++) {
-    const load = loadPerMin[index];
-    console.log('load', load)
+loadPerMin.forEach((load) => {
     setInterval(checkLoad(load), 60000)
-}
-
-console.log(server)
-
+})
